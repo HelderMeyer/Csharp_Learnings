fc8bcb0d2884f89c860c9feb8370532c49fa9c88
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-16 08:56:06 -0300

    Backup das atividades

---
+++ Takeaways/Arrays/Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            Console.WriteLine(luckyNumbers[0]);
+
+            luckyNumbers[1] = 900;
+
+            Console.WriteLine(luckyNumbers[1]);
+
+            string[] friends = new string[5];
+            friends[0] = "Sarah";
+            friends[1] = "Filipe";
+            friends[2] = "Marcelo";
+            friends[3] = "Felipe";
+            friends[4] = "Alberto";
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Takeaways/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Helder Meyer";
+            Console.WriteLine(name.Length); // It will tell me how many characters
+                                            // are inside of this string
+            Console.WriteLine(name.ToUpper()); // It will convert my string content to uppercase
+            Console.WriteLine(name.ToLower()); // It will convert my string content to lowercase
+            Console.WriteLine(name.Contains("s")); // It will return true of false whether or not the phrase in "name" string contains the word "s"
+            Console.WriteLine(name[0]); // It will tell me what is the letter in the first position of the string "name"
+            Console.WriteLine(name.IndexOf("r")); // It'll tell me the position "index" where the "M" is
+            Console.WriteLine(name.Substring(7, 3)); // It'll get the words starting from the 7 position and it will get only 3 words
+            Console.ReadLine();
+        }
+    }
+}
