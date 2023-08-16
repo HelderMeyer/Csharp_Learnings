48786810f5bb62f5fd5c7d58c81622f6df2702e7
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-16 11:12:17 -0300

    WhileLoop

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            while (index <= 10)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            do
+            {
+                Console.WriteLine(index);
+            } while (index < 10);
+            Console.ReadLine();
+        }
+    }
+}
