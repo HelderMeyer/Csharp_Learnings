60c0bbd9e9ec74ea06d4713d1fb5ac6869477343
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-16 09:45:56 -0300

    New exercises

---
+++ MoreIfStatements/MoreIfStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreIfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter a number: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            num2 = Int32.Parse(Console.ReadLine());
+
+            if(GetMax(num1, num2) == num1)
+            {
+                Console.WriteLine(num1 + " > " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num2 + " > " + num1);
+            }
+
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+            return result;
+        }
+    }
+}
