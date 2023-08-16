5600da44fea52e5c40757c1b5d48d3ebe7b4e9c1
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-16 11:22:07 -0300

    Guessing Game

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Helder";
+            string guess = "";
+            while (guess != name)
+            {
+                Console.Write("What is my name: ");
+                guess = Console.ReadLine();
+            }
+            Console.WriteLine("You won!");
+            Console.ReadLine();
+        }
+    }
+}
