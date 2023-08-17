b9ff480fc75ebe5225ebe45ab4e3a1a8d3b2c8cf
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-17 08:24:55 -0300

    Guessing Game with Attempts

---
+++ GuessingGameDoWhile/GuessingGameDoWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Helder";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != name && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                Console.Write("What is my name: ");
+                guess = Console.ReadLine();
+                guessCount++;
+                Console.WriteLine("Attempt " +  guessCount + "/" + guessLimit);
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You lost.");
+            }
+            else
+            {
+            Console.WriteLine("You won!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
