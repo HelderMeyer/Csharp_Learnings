869c632c2afd38fe142f29a7e0d9a7576903a1c6
Helder Meyer <heldermeyer@sescbahia.com.br>
2023-08-17 08:32:32 -0300

    ForLoops

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = {10, 20, 30, 40, 50};
+            for(int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+            foreach (int i in luckyNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
