dae82273b7256a0b25711d81e3cf6210da089e2a
Helder Meyer <helder.meyer3010@gmail.com>
2023-08-17 09:24:38 -0300

    ExponentMethod

---
+++ ExponentMethod/ExponentMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExponentMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the base number: ");
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is the pow number: ");
+            int powNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(baseNumber + "^" + powNumber + " = " + GetPow(baseNumber, powNumber));
+
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNumber, int powNumber)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNumber; i++)
+            {
+                result = result * baseNumber;
+            }
+
+            return result;
+        }
+    }
+}
