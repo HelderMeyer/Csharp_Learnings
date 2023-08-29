abf07ba620f07e36c825d00b9c8953d2851a6bfa
Helder Meyer <helder.meyer3010@gmail.com>
2023-08-29 11:43:35 -0300

    More Exercises

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            Console.Write("How old are you: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and you are {1}.", name, age);
+
+            Console.ReadLine();
+        }
+    }
+}
