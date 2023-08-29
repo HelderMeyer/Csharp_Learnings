55dd0d073d436eaccb1e7f20fc565cde397aff35
Helder Meyer <helder.meyer3010@gmail.com>
2023-08-29 10:57:42 -0300

    Formatando valores com ToString

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bradesco2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num.ToString("F2"));
+            Console.WriteLine(num.ToString("C"));
+            Console.WriteLine(num.ToString("P"));
+            Console.ReadLine();
+        }
+    }
+}
